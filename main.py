3c4639eb2f33652a76a403872b98caec78610c17
andjelkadzida <andjelkadzida@gmail.com>
2022-04-09 20:34:42 +0200

    Added question for user. Changed way of conversion regarding the extension. Changed message for user.

--- main.py
+++ main.py
@@ -7,30 +7,38 @@ import xlsxwriter
 
 root = tk.Tk()
 root.title("Konvertor xyz, qtt i lst ekstenzija u Excel")
-root.iconbitmap(os.path.join(os.path.dirname(os.getcwd()), 'launcher.ico'))
+#root.iconbitmap(os.path.join(os.path.dirname(os.getcwd()), 'launcher.ico'))
 root.withdraw()
 
 
 def fileConversion():
-    qttToChange = filedialog.askopenfilename()
-    if qttToChange.endswith('.qtt') or (qttToChange.endswith('.xyz') or qttToChange.endswith('.lst')):
-        while not os.path.isfile(qttToChange):
-            print("Greska: " + qttToChange + " nije fajlidna putanja do fajla. Pokusajte ponovo...")
-            qttToChange = filedialog.askopenfilename()
-        df = pd.read_csv(qttToChange, sep='\t' or '\s*' or '\s+' or 't', engine='python', encoding='utf-8')
+    extensionToChange = filedialog.askopenfilename()
+    if extensionToChange.endswith('.qtt') or (extensionToChange.endswith('.xyz') or extensionToChange.endswith('.lst')):
+        while not os.path.isfile(extensionToChange):
+            print("Greska: " + extensionToChange + " nije validna putanja do fajla. Pokušajte ponovo...")
+            extensionToChange = filedialog.askopenfilename()
+        df = pd.read_csv(extensionToChange, sep='\t' or '\s*' or '\s+' or 't', engine='python', encoding='utf-8')
         df.rename(columns={'Unnamed: 0': ''}, inplace=True)
         df.rename(columns={'Unnamed: 33': ''}, inplace=True)
-        fileName = os.path.splitext(qttToChange)[0]
-        writer = pd.ExcelWriter(fileName + '.xlsx', engine='xlsxwriter')
-        df.to_excel(writer, engine='python', encoding='utf-8')
+        if extensionToChange.endswith('.lst') or (extensionToChange.endswith('qtt')):
+            fileName = os.path.splitext(extensionToChange)[0]
+            writer = pd.ExcelWriter(fileName + '.xlsx', engine='xlsxwriter')
+            df.to_excel(writer, engine='python', encoding='utf-8', index=False)
+        else:
+            fileName = os.path.splitext(extensionToChange)[0]
+            writer = pd.ExcelWriter(fileName + '.xlsx', engine='xlsxwriter')
+            df.to_excel(writer, engine='python', encoding='utf-8')
         writer.save()
         # df.to_excel(fileName + '.xlsx', index=False)
-        # os.remove(qttToChange)
+        # os.remove(extensionToChange)
         convertAgain()
 
     else:
-        tk.messagebox.showerror(title="Greška", message="Izabrali ste fajl koji nije .qtt, .xyz ili .lst")
-        convertAgain()
+        res = tk.messagebox.askquestion(title="Greška", message="Izabrali ste fajl koji nije .qtt, .xyz ili .lst.\nDa li želite da pokušate ponovo?", icon='error')
+        if res == 'yes':
+            convertAgain()
+        else:
+            sys.exit()
 
 
 def convertAgain():
